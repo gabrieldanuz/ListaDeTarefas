b514fdbae767fffcb4663c8a95620582d3b8cf18
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-10-04 21:07:18 -0300

    Primeiro de muitos

---
+++ src/components/TaskList/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+export default function TaskList(){
+    return (
+        <View style={styles.container}>
+           <TouchableOpacity>
+               
+           </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
