4f4687a8fe65d5bd188d4fe60ba869bdac2137a4
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-10-09 23:54:42 -0300

    Pronto!

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
 import React, {useState} from "react";
-import {Ionicons} from '@expo/vector-icons'
-import { StyleSheet, Text, View, 
-  SafeAreaView, StatusBar, 
-  TouchableOpacity, FlatList } from "react-native";
+import {Ionicons} from '@expo/vector-icons';
+import { StyleSheet, Text, 
+  View, SafeAreaView,
+   StatusBar, TouchableOpacity,
+    FlatList, Modal, TextInput } from "react-native";
+import * as Animatable from 'react-native-animatable';
 
 import TaskList from "./src/components/TaskList";
 
-export default function App() {
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
-  const [task, setTask] = useState([
-    {key: 1,task: 'Comprar pão'},
-    {key: 2,task: 'Trabalhar'},
-    {key: 3,task: 'Estudar'},
-    {key: 4,task: 'Morrer'}
-  ]);
+export default function App() {
+  const [task, setTask] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
+
+
+  function handleAdd(){
+    if(input === '') return;
+
+    const data = {
+      key: input,
+      task: input
+    };
+
+    setTask([...task, data]);
+    setOpen(false);
+    setInput('');
+
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#171d31" barStyle="light-content" />
+      
       <View style={styles.content}>
         <Text style={styles.title}>Minhas Tarefas</Text>
       </View>
@@ -27,12 +43,47 @@ export default function App() {
       showsHorizontalScrollIndicator={false}
       data={task}
       keyExtractor={ (item) => String(item.key)}
-      randerItem={({item}) => <TaskList data={item} />}
+      renderItem={({item}) => <TaskList data={item} />}
       />
 
-      <TouchableOpacity style={styles.fab}>
+      <Modal animationType="slide" transparent={false} visible={open}>
+        <SafeAreaView style={styles.modal}>
+          <View style={styles.modalHeader}>
+            <TouchableOpacity onPress ={() => setOpen(false)}>
+              <Ionicons style={{marginLeft: 5, marginRight: 5}} name="md-arrow-back" size={40} color="#FFF" />
+            </TouchableOpacity>
+              <Text style={styles.modalTitle}>Nova Tarefa</Text>
+          </View>
+
+          <Animatable.View style={styles.modalBody} animation="fadeInUp" useNativeDriver>
+            <TextInput 
+            multiline={true}
+            placeholderTextColor="#747474"
+            autoCorrect={false}
+            placeholder="O que precisa fazer hoje?"
+            style={styles.input}
+            value={input}
+            onChange={(texto) => setInput(texto)}
+            />
+
+            <TouchableOpacity style={styles.handleAdd} onPress={handleAdd}>
+              <Text style={styles.handleAddText}>Cadastrar</Text>
+            </TouchableOpacity>
+          </Animatable.View>
+
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatedBtn 
+      style={styles.fab}
+      useNativeDriver
+      animation="bounceInUp"
+      duration={1500}
+      onPress={ () => setOpen(true)}
+      >
+      
         <Ionicons name="ios-add" size={35} color="#FFF" />
-      </TouchableOpacity>
+      </AnimatedBtn>
 
     </SafeAreaView>
   );
@@ -67,5 +118,48 @@ const styles = StyleSheet.create({
       width: 1,
       height: 3,
     }
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: '#171d31',
+  },
+  modalHeader: {
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  modalTitle: {
+    marginLeft: 15,
+    fontSize: 23,
+    color: "#FFF",
+  },
+  modalBody: {
+    marginTop: 15,
+  },
+  input: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 30,
+    backgroundColor: '#FFF',
+    padding: 9,
+    height: 85,
+    textAlignVertical: 'top',
+    borderRadius: 5,
+    color: '#000',
+  },
+  handleAdd: {
+    backgroundColor: "#FFF",
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40,
+    borderRadius: 5,
+  },
+  handleAddText: {
+    fontSize: 20,
   }
 });
